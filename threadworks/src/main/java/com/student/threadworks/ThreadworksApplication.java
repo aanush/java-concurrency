a287369518788d7dbe81644705cccf15b0bc444f
aanush <aanushang@sapient.com>
2017-06-21 20:53:03 +0530

    [Anish Anushang] created new project: threadworks

---
+++ threadworks/src/main/java/com/student/threadworks/ThreadworksApplication.java
@@ -0,0 +1,8 @@
+package com.student.threadworks;
+
+public class ThreadworksApplication {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ threadworks/src/test/java/com/student/threadworks/ThreadworksApplicationTests.java
@@ -0,0 +1,5 @@
+package com.student.threadworks;
+
+public class ThreadworksApplicationTests {
+
+}
